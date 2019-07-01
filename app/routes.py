baf6ba91c11b84ac310602a764307a6e32bdae4d
Polina <pppzzz22229@gmail.com>
2019-07-02 01:28:13 +0300

    Last commit for today

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from app import app
 from app.forms import NearC_Form
-from flask import render_template
+from flask import render_template, redirect
 from Constellation import new_constellation
 from Constellation.dictC import rus_lat_dict as rus_lat_dict
 
 @app.route('/')
+def space():
+    form = NearC_Form()
+    const = new_constellation.one_step()
+    messages = [const]
+    image_name = rus_lat_dict[const] + '.png'
+    return render_template('main_page.html.j2', title = "Home", messages = messages, 
+        image_name = image_name, form = form)
+
 @app.route('/home', methods = ["GET", "POST"])
 def home():
     form = NearC_Form()
@@ -13,3 +21,7 @@ def home():
     image_name = rus_lat_dict[const] + '.png'
     return render_template('main_page.html.j2', title = "Home", messages = messages, 
         image_name = image_name, form = form)
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect('/home')
