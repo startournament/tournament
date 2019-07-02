4a1fa16e225c6e01f0ec22f29eef29a085804ab7
Polina <pppzzz22229@gmail.com>
2019-07-02 13:25:38 +0300

    Black background

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,6 @@ from Constellation import new_constellation
 from Constellation.dictC import rus_lat_dict as rus_lat_dict
 
 @app.route('/')
-def space():
-    form = NearC_Form()
-    const = new_constellation.one_step()
-    messages = [const]
-    image_name = rus_lat_dict[const] + '.png'
-    return render_template('main_page.html.j2', title = "Home", messages = messages, 
-        image_name = image_name, form = form)
-
 @app.route('/home', methods = ["GET", "POST"])
 def home():
     form = NearC_Form()
@@ -21,7 +13,3 @@ def home():
     image_name = rus_lat_dict[const] + '.png'
     return render_template('main_page.html.j2', title = "Home", messages = messages, 
         image_name = image_name, form = form)
-
-@app.route('/favicon.ico')
-def favicon():
-    return redirect('/home')
