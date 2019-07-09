3d19e79752fde4e0ea34707642df26001e4a68a2
Polina <pppzzz22229@gmail.com>
2019-07-09 11:21:15 +0300

    Font size

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 
 class second_game_const_form(FlaskForm):
-    text = StringField(validators = [DataRequired()])
+    text = TextAreaField(validators = [DataRequired()])
